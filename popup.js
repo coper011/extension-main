340e64924aa034e103fe80337600ef8a683ffd61
petey01 <angelazhong12@gmail.com>
2025-02-03 18:44:22 -0800

    random message generator

--- popup.js
+++ popup.js
@@ -1,26 +1,50 @@
-var i = 5;
-let isCounting = false;
-let intervalId;
+var retro = new Audio('bells.mp3');
+var click = new Audio('click.wav');
 
-function updateCounter() {
-    if (i >= 0 && isCounting) {
-        document.getElementById("output").textContent = "Value of i: " + i;
-        i--;
-    }else if(i < 0){
-        clearInterval(intervalId);
-        document.getElementById("output").textContent = "Timer done"
+document.addEventListener("DOMContentLoaded", function(){
+    const input = document.getElementById("myMessage");
+    const button = document.getElementById("save");
+    const jarList = document.getElementById("jar");
+    //load data
+    chrome.storage.local.get(["userArray"], function(result){
+        let userArray = result.userArray || [];
+        renderList(userArray);
+    });
+    //save message
+    button.addEventListener("click", function(){
+        chrome.storage.local.get(["userArray"], function(result){
+            let userArray = result.userArray || [];
+            userArray.push(input.value);
+            chrome.storage.local.set({userArray: userArray}, function(){
+                console.log("Loaded data: ", result);
+                click.play();
+                renderList(userArray);
+                input.value = "";
+            });
+        });
+    });
+    //display list
+    function renderList(messages){
+        jarList.innerHTML = "";
+        messages.forEach(message => {
+            let li = document.createElement("li");
+            li.textContent = message;
+            jarList.appendChild(li);
+        });
     }
-}
-function startCounter(){
-    isCounting = !isCounting;
-    if(isCounting){
-        setInterval(updateCounter, 1000)
-    }else{
-        clearInterval(intervalId);
-    }
-}
-document.addEventListener("DOMContentLoaded", function () {
-    updateCounter();
-    const button = document.getElementById("timerButton");
-    button.addEventListener("click", startCounter);
+    const chooseButton = document.getElementById("choose");
+    chooseButton.addEventListener("click", function(){
+        chrome.storage.local.get(["userArray"], function(result){
+            let userArray = result.userArray || [];
+            console.log("choose clicked");
+            console.log(typeof userArray);
+            console.log(userArray);
+            if(userArray.length == 0){
+                document.getElementById("randomDisplay").textContent = "Nothing in the jar yet!";
+                return;
+            }
+            let num = Math.floor(Math.random() * userArray.length);
+            document.getElementById("randomDisplay").textContent = userArray[num];
+        });
+    });
 });
