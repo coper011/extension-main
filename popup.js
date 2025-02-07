2b554a8b9feead0498a2d1d39929f86f9c9ff33b
petey01 <angelazhong12@gmail.com>
2025-02-07 11:53:08 -0800

    working pages

--- popup.js
+++ popup.js
@@ -3,15 +3,16 @@ var click = new Audio('assets/sfx/click.wav');
 var papertoss = new Audio('assets/sfx/papertoss.mp3');
 var paper1 = new Audio('assets/sfx/paper1.mp3');
 var cork = new Audio('assets/sfx/cork.mp3');
+var flip = new Audio('assets/sfx/pageflip.mp3');
 
 document.addEventListener("DOMContentLoaded", function(){
     const input = document.getElementById("myMessage");
     const button = document.getElementById("save");
-    const jarList = document.getElementById("jar");
+    //const jarList = document.getElementById("jar");
     //load data
     chrome.storage.local.get(["userArray"], function(result){
         let userArray = result.userArray || [];
-        renderList(userArray);
+        //renderList(userArray);
         console.log("data loaded");
     });
     //save message
@@ -27,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function(){
                     console.log("input set");
                     click.play();
                     papertoss.play();
-                    renderList(userArray);
+                    //renderList(userArray);
                     input.value = "";
                 });
             }
@@ -63,8 +64,11 @@ document.addEventListener("DOMContentLoaded", function(){
     noteBookButton.addEventListener("click", function(){
         chrome.storage.local.get(["userArray"], function(result){
             switchToNotebook();
+            let userArray = result.userArray || [];
+            renderNote(userArray, currentIndex);
         });
     });
+    /*
     const clearButton = document.getElementById("clear");
     clearButton.addEventListener("click", function(){
         chrome.storage.local.get(["userArray"], function(result){
@@ -75,15 +79,51 @@ document.addEventListener("DOMContentLoaded", function(){
                 console.log("List cleared");
             });
         });
+        
     });
+    */
     const backButton = document.getElementById("back");
-    backButton.addEventListener("click", function(){
-        switchToHello();
-    });
+    backButton.addEventListener("click", switchToHello);
     const backButton2 = document.getElementById("back2");
-    backButton2.addEventListener("click", function(){
-        switchToHello();
+    backButton2.addEventListener("click", switchToHello);
+
+    //pg numbers and arrows, text display
+    let currentIndex = 0;
+    chrome.storage.local.get(["userArray"], function(result){
+        let userArray = result.userArray || [];
+        renderNote(userArray, currentIndex);
     });
+    const pageLeft = document.getElementById("leftArrow");
+    pageLeft.addEventListener("click", function(){
+        console.log("left pg clicked");
+        flip.play();
+        chrome.storage.local.get(["userArray"], function(result){
+            let userArray = result.userArray || [];
+            currentIndex -= 1;
+            if (currentIndex < 0){
+                currentIndex = userArray.length - 1;
+            } 
+            renderNote(userArray, currentIndex);
+        });
+    });
+    const pageRight = document.getElementById("rightArrow");
+    pageRight.addEventListener("click", function(){
+        console.log("right pg clicked");
+        flip.play();
+        chrome.storage.local.get(["userArray"], function(result){
+            let userArray = result.userArray || [];
+            currentIndex += 1;
+            if (currentIndex > userArray.length - 1){
+                currentIndex = 0;
+            } 
+            renderNote(userArray, currentIndex);
+        });
+    });
+    function renderNote(userArray, idx){
+        document.getElementById("notebookText").textContent = userArray[idx].content;
+        document.getElementById("notebookDate").textContent = userArray[idx].day;
+        document.getElementById("pageNum").textContent = (idx+1).toString() + "/" + (userArray.length).toString();
+    }
 });
 function switchToRandomNote(){
     cork.play();
